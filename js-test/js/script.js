14857608e1f8b7515f374404952fa66b2f20ce67
NikitaPetrov01 <pudge123nikita@gmail.com>
2018-07-18 18:41:02 +0300

    Fixiki@

--- js-test/js/script.js
+++ js-test/js/script.js
@@ -2,20 +2,21 @@ var answers = ['Пятнадцать', 'Шаурма', 'Пиво'];
 var answer1 = prompt('Сколько ему лет?');
 var score=0;
 console.log(answer1);
-if (answer1==answers[1]) {
+if (answer1==answers[0]) {
     score=score+1;
 };
 var answer2 = prompt('Что он ест?');
 console.log(answer2);
-if (answer2==answers[2]) {
+if (answer2==answers[1]) {
     score=score+1;
 };
 var answer3 = prompt('Что он пьёт?');
 console.log(answer3);
-if (answer3==answers[3]) {
+if (answer3==answers[2]) {
     score=score+1;
 };
-alert (score);
+alert ('Вы набрали ' +score+ ' балла!');
+
 
 
 
